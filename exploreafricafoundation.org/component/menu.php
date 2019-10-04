4f5421003fb3109fd3aeccae72592c41603536b6
zubbey <com.zubbey@icloud.com>
2019-10-04 16:57:59 +0100

    initial changes

--- exploreafricafoundation.org/component/menu.php
+++ exploreafricafoundation.org/component/menu.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="no-js">
+<html lang="en">
 <head>
     <!-- Mobile Specific Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -25,30 +25,31 @@
     <link rel="apple-touch-icon" sizes="144x144" href="./img/fav/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="./img/fav/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="./img/fav/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="../img/fav/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./img/fav/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./img/fav/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="./img/fav/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./img/fav/favicon-16x16.png">
     <link rel="manifest" href="./img/fav/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 
     <!--
     CSS
     ============================================= -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,900,900i&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/magnific-popup.css">
     <link rel="stylesheet" href="./css/animate.min.css">
-    <link rel="stylesheet" href="./css/main.css">
+<!--    <link rel="stylesheet" href="./css/main.css">-->
     <link rel="stylesheet" href="./css/custom.css">
     <link rel="stylesheet" href="./css/theme.css">
 </head>
-<body>
+<body class="body">
+<div class="container">
+    <section class="navbar">
 <nav class="navbar navbar-expand-lg navbar-light bg-light order-0">
     <a class="navbar-brand" href="#">
-        <img src="./img/EAF_icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Explore Africa Foundation
+        <img src="./img/EAF_icon.png" width="30" height="30" class="d-inline-block align-top" alt="explore africa logo">
+        Explore Africa <span>Foundation</span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -56,20 +57,20 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">News <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Our Work</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="#">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link" href="#">Join Us</a>
             </li>
         </ul>
     </div>
-    <form class="form-inline">
-        <button class="btn btn-outline-success" type="button"><a href="../donate.php">Donate Now</a> <span class="glyphicon glyphicon-heart"></span> </button>
-    </form>
+    <button class="btn default-big-button" type="button"><a href="../donate.php">Donate Now <i class="fa fa-heart"></i></a></button>
 </nav>
+    </section>
+</div>
