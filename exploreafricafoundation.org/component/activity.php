7e77f63ddd5b69ba40be1d6c361a6569f67a8cf1
zubbey <com.zubbey@icloud.com>
2019-10-06 23:33:57 +0100

    made new changes

--- exploreafricafoundation.org/component/activity.php
+++ exploreafricafoundation.org/component/activity.php
@@ -1,32 +1,37 @@
 <div class="section-3">
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="jumbotron">
-                <h1 class="big-heading">Let's Do <br> Something About <br> This Statistics</h1>
-                <p class="medium-heading">10.5 Million Children Out of School In Nigeria As Revealed By <span class="important">Adamu Hussaini,</span> The Permanent Secretary Of Country's Ministry Of Education is Not A Joke. <a href="#" class="important">Learn More..</a></p>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="jumbotron">
+                    <h1 class="big-heading">Let's Do <br>Something About <br> This Statistics</h1>
+                    <p class="medium-heading">10.5 Million Children Out of School In Nigeria As Revealed By <span
+                                class="important">Adamu Hussaini,</span> The Permanent Secretary Of Country's Ministry
+                        Of Education is Not A Joke. <a href="#" class="important">Learn More..</a></p>
+                </div>
+            </div>
+            <div class="col image-col">
             </div>
         </div>
-        <div class="col image-col">
-        </div>
-    </div>
 
-</div>
+    </div>
 </div>
 <div class="section-4">
     <div class="container">
         <h1 class="heading">Popular Campaign</h1>
 
         <!-- Flickity HTML init -->
-        <div class="carousel" data-flickity='{ "cellAlign": "left", "contain": true, "groupCells": 1, "autoPlay": 5000 }'>
+        <div class="carousel"
+             data-flickity='{ "cellAlign": "left", "contain": true, "groupCells": true, "autoPlay": 5000 }'>
             <div class="carousel-cell">
                 <div class="card mb-3">
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Back to School Support Project </h5>
-                        <img src="https://i.imgur.com/1plCNZL.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> Explore Africa Foundation is currently seeking for financial assistance to supporting indigent children under the BACK TO SCHOOL project!</p>
+                        <img src="https://i.imgur.com/1plCNZL.jpg" class="card-img-top 1" alt="...">
+                        <p class="card-text 1 pt-3"> Explore Africa Foundation is currently seeking for financial
+                            assistance to supporting indigent children under the BACK TO SCHOOL project!</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 2 Weeks ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 2 Weeks ago</small> <span
+                                    class="small" id="more1" onclick="clickedReadmore1()">Read More...</span></p>
                     </div>
                 </div>
             </div>
@@ -34,10 +39,36 @@
                 <div class="card mb-3">
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Younth Konnect for Peace Network </h5>
-                        <img src="https://i.imgur.com/IXt9l1T.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> When television was young, there was a hugely popular show based on the still popular fictional character.</p>
+                        <img src="https://i.imgur.com/IXt9l1T.jpg" class="card-img-top 2" alt="...">
+                        <p class="card-text 2 pt-3"> When television was young, there was a hugely popular show based on
+                            the still popular fictional character.</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small> <span
+                                    class="small" id="more2" onclick="clickedReadmore2()">Read More... </span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Free Health Care Support Service</h5>
+                        <img src="https://i.imgur.com/dTTYxv6.jpg" class="card-img-top 3" alt="...">
+                        <p class="card-text 3 pt-3"> our team of professional health workers provide a free medical outreach some riverine communities in Niger Delta Region.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small> <span
+                                    class="small" id="more3" onclick="clickedReadmore3()">Read More... </span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Free Skill Acquisition and Entrepreneurship Training</h5>
+                        <img src="https://i.imgur.com/gtW8rwl.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> The labour market has gone beyond your certificate or degree, many graduate have not been employed in organization and companies because they dont have employable skill in ICT and Computers.  </p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 2 year ago</small> <span
+                                    class="small" id="more4" onclick="clickedReadmore4()">Read More... </span></p>
                     </div>
                 </div>
             </div>
@@ -46,31 +77,41 @@
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Young leaders mentoring programme </h5>
                         <img src="https://i.imgur.com/GOxvqQo.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3">  Some people claim leaders are born, others maintain leaders are made. Whether ‘born’ or ‘made’, the crux of the matter is neither born nor made, rather the need of strong leaders across the wide spectrum of societal existence. .</p>
+                        <p class="card-text pt-3"> Some people claim leaders are born, others maintain leaders are made.
+                            Whether ‘born’ or ‘made’, the crux of the matter is neither born nor made, rather the need
+                            of strong leaders across the wide spectrum of societal existence. .</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 2 year ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 2 year ago</small> <span
+                                    class="small" id="more5" onclick="clickedReadmore5()">Read More... </span></p>
                     </div>
                 </div>
             </div>
             <div class="carousel-cell">
                 <div class="card mb-3">
                     <div class="card-body">
-                        <h5 class="medium-heading pb-3">Social innovators forum	</h5>
+                        <h5 class="medium-heading pb-3">Social innovators forum </h5>
                         <img src="https://i.imgur.com/uE356Ap.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3">  To feign ignorance about the changing dimensions of our society today is a preparation towards unavoidable regrets. These changes are not self created; they are outcomes of innovative thinking. </p>
+                        <p class="card-text pt-3"> To feign ignorance about the changing dimensions of our society today
+                            is a preparation towards unavoidable regrets. These changes are not self created; they are
+                            outcomes of innovative thinking. </p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 8 Month ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 8 Month ago</small> <span
+                                    class="small" id="more6" onclick="clickedReadmore6()">Read More... </span></p>
                     </div>
                 </div>
             </div>
             <div class="carousel-cell">
                 <div class="card mb-3">
                     <div class="card-body">
-                        <h5 class="medium-heading pb-3">Renovation and upgrade of community schools libraries and laboratories </h5>
+                        <h5 class="medium-heading pb-3">Renovation and upgrade of community schools libraries and
+                            laboratories </h5>
                         <img src="https://i.imgur.com/liZPXdc.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> Explore Africa Foundation plans to setup a project developmental fund to cater for renovation and upgrade of community schools majorly libraries, laboratories, and latrines.</p>
+                        <p class="card-text pt-3"> Explore Africa Foundation plans to setup a project developmental fund
+                            to cater for renovation and upgrade of community schools majorly libraries, laboratories,
+                            and latrines.</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small> <span
+                                    class="small" id="more7" onclick="clickedReadmore7()">Read More... </span></p>
                     </div>
                 </div>
             </div>
@@ -79,20 +120,27 @@
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Educational support and scholarship programme </h5>
                         <img src="https://i.imgur.com/HwonUdH.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> We are equally help-in-time organization particularly in the area of education. One of the ways we support educational pursuit of young peopl is through our scholarship programmes.</p>
+                        <p class="card-text pt-3"> We are equally help-in-time organization particularly in the area of
+                            education. One of the ways we support educational pursuit of young peopl is through our
+                            scholarship programmes.</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 month ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 month ago</small> <span
+                                    class="small" id="more8" onclick="clickedReadmore8()">Read More... </span></p>
                     </div>
                 </div>
             </div>
             <div class="carousel-cell">
                 <div class="card mb-3">
                     <div class="card-body">
-                        <h5 class="medium-heading pb-3">Summer ICT and Personal development bootcamp  </h5>
+                        <h5 class="medium-heading pb-3">Summer ICT and Personal development bootcamp </h5>
                         <img src="https://i.imgur.com/9gvfGgE.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> Explore Africa Foundation has a strong and sound IT background and feels strongly indebted in that area. We package summer ICT courses for fresher’s and already practicing young people to get them sufficiently informed and positioned to profit from the huge opportunities that the IT industry offers.</p>
+                        <p class="card-text pt-3"> Explore Africa Foundation has a strong and sound IT background and
+                            feels strongly indebted in that area. We package summer ICT courses for fresher’s and
+                            already practicing young people to get them sufficiently informed and positioned to profit
+                            from the huge opportunities that the IT industry offers.</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 weeks ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 weeks ago</small> <span
+                                    class="small" id="more9" onclick="clickedReadmore9()">Read More... </span></p>
                     </div>
                 </div>
             </div>
@@ -101,9 +149,12 @@
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Schools Educational outreach </h5>
                         <img src="https://i.imgur.com/xFJpgmA.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3">  We are passionate about the young people and this is evident in the chunk of time we allot to the different programmes connected to them all through the year. </p>
+                        <p class="card-text pt-3"> We are passionate about the young people and this is evident in the
+                            chunk of time we allot to the different programmes connected to them all through the
+                            year. </p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 4 month ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 4 month ago</small> <span
+                                    class="small" id="more10" onclick="clickedReadmore10()">Read More... </span></p>
                     </div>
                 </div>
             </div>
@@ -112,13 +163,27 @@
                     <div class="card-body">
                         <h5 class="medium-heading pb-3">Youth Enterprise Development </h5>
                         <img src="https://i.imgur.com/Pq5LQJN.jpg" class="card-img-top" alt="...">
-                        <p class="card-text pt-3"> In this age and time, we need more balance teachings than abstinence instructions particularly in the area of business.</p>
+                        <p class="card-text pt-3"> In this age and time, we need more balance teachings than abstinence
+                            instructions particularly in the area of business.</p>
                         <div class="divider"></div>
-                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 year ago</small> <span class="small"><a href="#">Learn More...</a> </span></p>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 3 year ago</small> <span
+                                    class="small" id="more11" onclick="clickedReadmore11()">Read More... </span></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div class="section-5">
+    <div class="container">
+        <div class="row">
+            <div class="col image-col2">
+                <div class="jumbotron">
+                    <h1 class="big-heading">Do you Know?</h1> <span class="medium-heading">With 30,000 Naira You Can Send a Child Back to School</span>
+                    <p class="p">Join Us On Our Back to School Support Project <a href="#" class="important small">Learn More..</a></p>
+                </div>
+            </div>
+        </div>
 
+    </div>
 </div>
--- exploreafricafoundation.org/component/footer.php
+++ exploreafricafoundation.org/component/footer.php
@@ -20,8 +20,153 @@
                 $('.navbar-toggler.2').addClass('d-none');
             }
         });
+        $('#more1').click(function () {
+            $('.card-text.1').toggleClass('show-more');
+            $('.card-img-top.1').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more2').click(function () {
+            $('.card-text.2').toggleClass('show-more');
+            $('.card-img-top.2').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more3').click(function () {
+            $('.card-text.3').toggleClass('show-more');
+            $('.card-img-top.3').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more4').click(function () {
+            $('.card-text.4').toggleClass('show-more');
+            $('.card-img-top.4').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more5').click(function () {
+            $('.card-text.5').toggleClass('show-more');
+            $('.card-img-top.5').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more6').click(function () {
+            $('.card-text.6').toggleClass('show-more');
+            $('.card-img-top.6').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more7').click(function () {
+            $('.card-text.7').toggleClass('show-more');
+            $('.card-img-top.7').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more8').click(function () {
+            $('.card-text.8').toggleClass('show-more');
+            $('.card-img-top.8').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more9').click(function () {
+            $('.card-text.9').toggleClass('show-more');
+            $('.card-img-top.9').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more10').click(function () {
+            $('.card-text.10').toggleClass('show-more');
+            $('.card-img-top.10').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+        $('#more11').click(function () {
+            $('.card-text.11').toggleClass('show-more');
+            $('.card-img-top.11').toggleClass('crop-img');
+            // $(this).html('Read More..');
+        });
+
     });
 
+    function clickedReadmore1() {
+        var a = document.getElementById("more1");
+        if (a.innerHTML === "Show Less.."){
+            a.innerHTML = "Read More..";
+        } else {
+            a.innerHTML = "Show Less..";
+        }
+
+    }
+    function clickedReadmore2() {
+        var b = document.getElementById("more2");
+        if (b.innerHTML === "Show Less.."){
+            b.innerHTML = "Read More..";
+        } else {
+            b.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore3() {
+        var c = document.getElementById("more3");
+        if (c.innerHTML === "Show Less.."){
+            c.innerHTML = "Read More..";
+        } else {
+            c.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore4() {
+        var d = document.getElementById("more4");
+        if (d.innerHTML === "Show Less.."){
+            d.innerHTML = "Read More..";
+        } else {
+            d.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore5() {
+        var e = document.getElementById("more5");
+        if (e.innerHTML === "Show Less.."){
+            e.innerHTML = "Read More..";
+        } else {
+            e.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore6() {
+        var f = document.getElementById("more6");
+        if (f.innerHTML === "Show Less.."){
+            f.innerHTML = "Read More..";
+        } else {
+            f.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore7() {
+        var g = document.getElementById("more7");
+        if (g.innerHTML === "Show Less.."){
+            g.innerHTML = "Read More..";
+        } else {
+            g.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore8() {
+        var h = document.getElementById("more8");
+        if (h.innerHTML === "Show Less.."){
+            h.innerHTML = "Read More..";
+        } else {
+            h.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore9() {
+        var i = document.getElementById("more9");
+        if (i.innerHTML === "Show Less.."){
+            i.innerHTML = "Read More..";
+        } else {
+            i.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore10() {
+        var j = document.getElementById("more10");
+        if (j.innerHTML === "Show Less.."){
+            j.innerHTML = "Read More..";
+        } else {
+            j.innerHTML = "Show Less..";
+        }
+    }
+    function clickedReadmore11() {
+        var k = document.getElementById("more11");
+        if (k.innerHTML === "Show Less.."){
+            k.innerHTML = "Read More..";
+        } else {
+            k.innerHTML = "Show Less..";
+        }
+    }
     (function($) {
         "use strict";
 
@@ -31,7 +176,19 @@
 
     })(jQuery);
 
+    // MEDIA QUERY WITH JAVASCRIPT
+    $(window).resize(function(){
 
+        if ($(window).width() <= 480) {
+
+            $('.carousel-caption').removeClass('d-none');
+            $('.caption-heading').css('font-size', '20px');
+            $('.caption-body').css('font-size', '12px');
+            $('.caption-body a.button-link').css('font-size', '12px');
+
+        }
+
+    });
 
 </script>
 </body>
