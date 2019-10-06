fe8e86108ecdac6ae65f931adbff806252402148
zubbey <com.zubbey@icloud.com>
2019-10-06 04:22:18 +0100

    new changes

--- exploreafricafoundation.org/component/activity.php
+++ exploreafricafoundation.org/component/activity.php
@@ -8,15 +8,99 @@
 </div>
 <div class="section-4">
     <div class="container">
-        <h1>Popular Campagne</h1>
+        <h1 class="heading">Popular Campaign</h1>
 
         <!-- Flickity HTML init -->
         <div class="carousel" data-flickity='{ "autoPlay": true }'>
-            <div class="carousel-cell"></div>
-            <div class="carousel-cell"></div>
-            <div class="carousel-cell"></div>
-            <div class="carousel-cell"></div>
-            <div class="carousel-cell"></div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Younth Konnect for Peace Network </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> When television was young, there was a hugely popular show based on the still popular fictional character.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                        <button class="btn" type="button"><a class="button-link" href="../ykpn.exploreafricafoundation.orgp">Learn More..</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Young leaders mentoring programme </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3">  Some people claim leaders are born, others maintain leaders are made. Whether ‘born’ or ‘made’, the crux of the matter is neither born nor made, rather the need of strong leaders across the wide spectrum of societal existence. .</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Social innovators forum	</h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3">  To feign ignorance about the changing dimensions of our society today is a preparation towards unavoidable regrets. These changes are not self created; they are outcomes of innovative thinking. </p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Renovation and upgrade of community schools libraries and laboratories </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> Explore Africa Foundation plans to setup a project developmental fund to cater for renovation and upgrade of community schools majorly libraries, laboratories, and latrines.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Educational support and scholarship programme </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> We are equally help-in-time organization particularly in the area of education. One of the ways we support educational pursuit of young peopl is through our scholarship programmes.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Summer ICT and Personal development bootcamp  </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> Explore Africa Foundation has a strong and sound IT background and feels strongly indebted in that area. We package summer ICT courses for fresher’s and already practicing young people to get them sufficiently informed and positioned to profit from the huge opportunities that the IT industry offers.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Schools Educational outreach </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3">  We are passionate about the young people and this is evident in the chunk of time we allot to the different programmes connected to them all through the year. </p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="medium-heading pb-3">Youth Enterprise Development </h5>
+                        <img src="https://i.imgur.com/t12SQVn.jpg" class="card-img-top" alt="...">
+                        <p class="card-text pt-3"> In this age and time, we need more balance teachings than abstinence instructions particularly in the area of business.</p>
+                        <div class="divider"></div>
+                        <p class="card-text pt-2"><small class="text-muted">Last updated 1 year ago</small></p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
