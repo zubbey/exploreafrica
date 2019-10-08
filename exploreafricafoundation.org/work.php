e7f99c17607214de826360aa439b8d98fe4e32ad
zubbey <com.zubbey@icloud.com>
2019-10-08 05:55:43 +0100

    ready to deploy v0.1

---
+++ exploreafricafoundation.org/work.php
@@ -0,0 +1,356 @@
+<?php
+include('component/menu.php');
+?>
+
+<section class="popular-courses-area section-gap courses-page">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-70 col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10">Popular Campaign</h1>
+                    <p>While we do not claim the position of Jack of all trades, we admit the disposition that lends us growth allowance to intersect and interface with all sectors of the economy starting from Nigeria.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l1.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 28 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Youth Konnect for Peace Network
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l0.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 35 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Electral Violence and Drug Abuse Campaign
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l2.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 57 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Young leaders mentoring programme
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l3.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 45 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Social innovators forum
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 mt-10">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l6.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 310 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Renovation and upgrade of community schools libraries and laboratories
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l10.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 215 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Educational support and scholarship programme
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l4.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 21 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Summer ICT and Personal development bootcamp
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l11.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 09 <span class="lnr lnr-bubble"></span>35</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Schools Educational outreach
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l4.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 380 <span class="lnr lnr-bubble"></span>15</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Summer ICT and Personal Development Bootcamp
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <!-- ========================Hidden Classes to be loaded ================================================================== -->
+
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l8.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 35 <span class="lnr lnr-bubble"></span>5</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Youth Enterprise Development
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l7.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 56 <span class="lnr lnr-bubble"></span>100</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Young Leaders club
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l13.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 53 <span class="lnr lnr-bubble"></span>30</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Human capacity development
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l5.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 315 <span class="lnr lnr-bubble"></span>21</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Skills acquisition programme
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l12.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 25 <span class="lnr lnr-bubble"></span>3</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Capital creation scheme
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-3 col-md-6 d-none">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <img class="img-fluid" src="img/l9.jpg" alt="">
+                    </div>
+                    <div class="meta d-flex justify-content-between">
+                        <p><span class="lnr lnr-users"></span> 42 <span class="lnr lnr-bubble"></span>56</p>
+                        <h4></h4>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="course-details.html">
+                        <h4>
+                            Sector revamp and positioning
+                        </h4>
+                    </a>
+                    <p>
+                        When television was young, there was a hugely popular show based on the still popular fictional characte
+                    </p>
+                </div>
+            </div>
+            <a href="#" class="primary-btn text-uppercase mx-auto">Load More Programme</a>
+        </div>
+    </div>
+</section>
+<!-- End popular-courses Area -->
+
+<?php
+include('component/footer.php');
+?>
